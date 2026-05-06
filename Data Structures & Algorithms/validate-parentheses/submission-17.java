2f28668b37c4175fc94d5b9968f8595340ddc1bc
Avdhesh Kumar <kavdhesh78@gmail.com>
2026-05-07 00:34:57 +0530

    Add: validate-parentheses - submission-17

---
+++ Data Structures & Algorithms/validate-parentheses/submission-17.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> parentheses = new Stack<>();
+
+        if (s.length() < 2) {
+            return false;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                parentheses.push(ch);
+            } else if (!parentheses.isEmpty()) {
+                char popItem = parentheses.pop();
+                if ((popItem == '{' && ch != '}') || (popItem == '[' && ch != ']')
+                    || (popItem == '(' && ch != ')')) {
+                    return false;
+
+                }
+
+            } else {
+                return false;
+            }
+        }
+        return parentheses.isEmpty();
+    }
+}
